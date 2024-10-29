2285e04d1e575b9c70d4d65265e91906340c62b7
Hethii <ezertobi@gmail.com>
2024-10-29 05:25:10 +0100

    latest

--- src/Imports.sol
+++ src/Imports.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.20;
 
 import "solmate/tokens/ERC20.sol";
 
-contract Token is ERC20("name", "symbol", 18) {
-    
-}
+contract Token is ERC20("name", "symbol", 18) {}
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TextOz is Ownable {}
